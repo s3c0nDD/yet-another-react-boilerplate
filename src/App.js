9aea0b8eb8ae04bf22c9995b8632bbb3bbf3c5b2
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-26 15:52:01 +0200

    add bootstrap 3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
 class App extends Component {
   render() {
+    const pStyle = {
+        textAlign: 'center'
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+            <Navbar inverse fixedTop>
+                <Grid>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="/">React App</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                </Grid>
+            </Navbar>
+            <Jumbotron>
+                <Grid>
+                    <h1>Welcome to React</h1>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p style={pStyle}>
+                        <Button
+                            bsStyle="success"
+                            bsSize="large"
+                            href="http://react-bootstrap.github.io/components.html"
+                            target="_blank">
+                            View React Bootstrap Docs
+                        </Button>
+                    </p>
+                </Grid>
+            </Jumbotron>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
