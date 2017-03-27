c118a67c393fb20e170cccd557224eeb190d391f
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-27 15:28:06 +0200

    add some navigation, differents methods

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
 const Home = (props) => {
-  console.log(props);
   return <h1>Home</h1>;
 };
 
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link replace to="/contact">Contact</Link>
+  </nav>
+);
 
 const App = () => (
   <Router>
     <div>
+      <Links />
       <Route exact path="/" component={Home}/>
-      {/*<Route path="/about" render={() => <h1>About</h1>}/>*/}
-      <Route
-        path="/about"
-        children={({match}) => match && <h1>About</h1>}/>
+      <Route path="/about" render={() => <h1>About</h1>}/>
+      <Route path="/contact" render={() => <h1>Contact</h1>}/>
     </div>
   </Router>
 );
